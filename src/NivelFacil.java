60184f5ee4df7e1b36a3f557956961462245dd5e
Athay <abdieldeathayde67@gmail.com>
2023-12-29 15:46:49 -0300

    refatorando codigo jogo adivinhe o numero

--- src/NivelFacil.java
+++ src/NivelFacil.java
@@ -7,7 +7,7 @@ public class NivelFacil {
     int pontos = 25;
 
     void jogar() {
-        ArrayList<Integer> numerosAleatorios = new ArrayList<>();
+        int numerosAleatorios;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um numero entre 0 e 30: ");
@@ -17,34 +17,33 @@ public class NivelFacil {
 
         int upperbound = 10;
         Random random = new Random();
-        numerosAleatorios.add(random.nextInt(upperbound));
+        numerosAleatorios = random.nextInt(upperbound);
 
 
-        while (pontos != 0) {
-            for (Integer numerosAleatorio : numerosAleatorios) {
-
-                if (numero != numerosAleatorio) {
-                    --pontos;
-                    System.out.println("Numero incorreto! Tente novamente \nPontuação: " + pontos);
 
 
-                } else {
-                    ++pontos;
-                    System.out.println("Você acertou! Numero: " + numero + "\nPontuação: " + pontos);
+        if (numero != numerosAleatorios) {
+            --pontos;
+            System.out.println("Numero incorreto! Tente novamente \nPontuação: " + pontos);
 
-                }
-            }
-            System.out.println("Digite um numero entre 0 e 30: ");
-            numero = sc.nextInt();
 
-            if (numero != 0) {
-                jogar();
-            } else {
-                System.out.println("Encerrando jogo!");
-            }
+        } else {
+            ++pontos;
+            System.out.println("Você acertou! Numero: " + numero + "\nPontuação: " + pontos);
 
+        }
+
+        System.out.println("Digite um numero entre 0 e 30: ");
+        numero = sc.nextInt();
+
+        if (numero != 0) {
+            jogar();
+        } else {
+            System.out.println("Encerrando jogo!");
         }
 
     }
 
 }
+
+
